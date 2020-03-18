e59b50115fd52a55801fef6b8a3667306968e2b2
Jasmin-Mediani <molihua1992@gmail.com>
2020-03-18 09:23:24 +0100

    sistemato camelCase a datiInArrivo

--- js/main.js
+++ js/main.js
@@ -11,8 +11,8 @@ $(document).ready(function () {
     $.ajax({
         url: "https://flynn.boolean.careers/exercises/api/array/music",
         method: "GET",
-        success: function (datiInarrivo) {
-            var tuttiGliAlbum = datiInarrivo.response; //è un array di oggetti.  Il singolo album (l'esimo) sarà i. 
+        success: function (datiInArrivo) {
+            var tuttiGliAlbum = datiInArrivo.response; //è un array di oggetti.  Il singolo album (l'esimo) sarà i. 
 
             for (var i = 0; i < tuttiGliAlbum.length; i++) {
                 var singoloAlbum = tuttiGliAlbum[i];
